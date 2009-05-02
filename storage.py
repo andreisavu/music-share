e9c638d084fe14c9ad528dc47d8f758f2fbb6111
Andrei Savu <savu.andrei@gmail.com>
2009-05-02 23:43:38 +0300

    regexp input data cleanup

--- storage.py
+++ storage.py
@@ -1,12 +1,18 @@
 
-import web
+import web, re
 
 def db_save(info, db):
+	""" Save data in database """
 	return db.insert('ms_files', filename=info['FILENAME'], 
 		album=info['ALBUM'], artist=info['ARTIST'], 
 		title=info['TITLE'], year=info['YEAR'])
 
+def clean(v):
+	""" Remove any not visible chars """
+	return re.sub('[^\w. ]+', '', v)
+
 def add_missing_fields(info):
+	""" Add to the info structure the missing fields """
 	if not 'ALBUM' in info:
 		info['ALBUM'] = ''
 	if not 'ARTIST' in info:
@@ -15,9 +21,15 @@ def add_missing_fields(info):
 		info['TITLE'] = ''
 	if not 'YEAR' in info:
 		info['YEAR'] = 0
+
+	info['ALBUM'] = clean(info['ALBUM'])
+	info['ARTIST'] = clean(info['ARTIST'])
+	info['TITLE'] = clean(info['TITLE'])
+
 	return info
 
 def save(info, fp, db):
+	""" Save track info in database """
 	info = add_missing_fields(info)
 	id = db_save(info, db)
 	dest = "static/upload/%d.mp3" % id
